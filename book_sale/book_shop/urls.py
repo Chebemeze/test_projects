184e0b3449b4f95ef1f794ee22083ccb35b84003
Chebemeze <chebemeze@gmail.com>
2026-02-16 14:42:24 +0100

    updated error template, views and url

--- book_sale/book_shop/urls.py
+++ book_sale/book_shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import HelloView, BookView, CreateBookView
+from .views import HelloView, BookView, CreateBookView, UpdateBookView, SignupView
 
 from . import views
 
@@ -8,5 +8,7 @@ urlpatterns = [
   path("hello/", HelloView.as_view(), name="hello"),
   path("all-books/", BookView.as_view(), name = "all_books"),
   path("create-books/", CreateBookView.as_view(), name = "create_book"),
+  path("update/<int:pk>/", UpdateBookView.as_view(), name= "update_book"),
+  path("sign-up/", SignupView.as_view(), name= "sign_up")
   
 ]
--- book_sale/book_shop/views.py
+++ book_sale/book_shop/views.py
@@ -1,8 +1,9 @@
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView
 from .models import Book
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # Create your views here.
 
 def welcome(request):
@@ -23,3 +24,37 @@ class CreateBookView(CreateView):
   fields = ('Author', 'title', 'SNB', 'is_available',)
   template_name = "book_shop/create.html"
   success_url = reverse_lazy('home')
+
+class SignupView(CreateView):
+  model = Book
+  form_class = UserCreationForm
+  template_name = "book_shop/create.html"
+  success_url = reverse_lazy('home')
+
+# A class based view to update an existing record
+# you don't have to specify the context_object_name for CreateView
+# and UpdateView because the object name by default is form
+# when using CreatView and UpdateView the fields to modify must be specified in the class
+class UpdateBookView(UpdateView):
+  model = Book
+  fields = ('Author', 'title', 'SNB', 'is_available',)
+  template_name = "book_shop/create.html"
+  success_url= reverse_lazy('all_books')
+
+
+  def get_object(self, queryset = None):
+    try:
+      return super().get_object(queryset)
+    except Http404:
+      return None
+    
+  def get(self, request, *args, **kwargs):
+    self.object = self.get_object()
+    if self.object is None:
+      context = {
+        'error_message': "The book you are looking for does not exist, enter a valid pk next time",
+      }
+      return render (request, 'book_shop/not_found.html', context)
+    return super().get(request, *args, **kwargs)
+
+
