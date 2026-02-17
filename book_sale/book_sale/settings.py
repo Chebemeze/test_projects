845392db55daa3099e7931cd39173f24bdbcf3ed
Chebemeze <chebemeze@gmail.com>
2026-02-17 03:39:52 +0100

    deleted account app

--- book_sale/book_sale/settings.py
+++ book_sale/book_sale/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book_shop.apps.BookShopConfig',
+
 ]
 
 MIDDLEWARE = [
--- book_sale/book_sale/urls.py
+++ book_sale/book_sale/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('book_shop.urls')),
     path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
 ]
