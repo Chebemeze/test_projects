7dc7838620eb896edad7778456a1234d26e72398
Chebemeze <chebemeze@gmail.com>
2026-02-19 14:02:03 +0100

    added css styling for book model

--- book_sale/book_shop/forms.py
+++ book_sale/book_shop/forms.py
@@ -8,7 +8,7 @@ class BookForm(forms.ModelForm):
     model = Book
     fields = ['title', 'SNB', 'Author', 'is_available']
     widgets = {
-      "title": forms.TextInput(attrs={"class":"input"}),
+      "title": forms.TextInput(attrs={"class":"input", "placeholder": "Enter title"}),
       "SNB": forms.TextInput(attrs={"class":"input"}),
       "Author": forms.TextInput(attrs={"class":"input"}),
       "is_available": forms.CheckboxInput(attrs={"class":"input"})
